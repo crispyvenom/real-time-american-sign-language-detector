caf579aa7cae7c1a39d069c22fbba78e9ead7775
Anubhav Majumdar <anubhavmajumdar1706@gmail.com>
2023-12-29 13:30:12 +0530

    mediapipe still not working but model is functional in localhost

--- 2_Model.py
+++ 2_Model.py
@@ -1,6 +1,5 @@
 import cv2
 import streamlit as st
-import mediapipe as mp
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 from cvzone.HandTrackingModule import HandDetector
 from cvzone.ClassificationModule import Classifier
